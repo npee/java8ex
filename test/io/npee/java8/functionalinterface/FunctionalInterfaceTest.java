0a8b836751feb386db1fb015c5b42330a2c39f2c
npee <jmc4664@gmail.com>
2021-12-11 23:56:52 +0900

    Function compose 테스트

--- test/io/npee/java8/functionalinterface/FunctionalInterfaceTest.java
+++ test/io/npee/java8/functionalinterface/FunctionalInterfaceTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.function.Function;
 import org.junit.jupiter.api.Test;
 
 class FunctionalInterfaceTest {
@@ -20,4 +21,12 @@ class FunctionalInterfaceTest {
         assertEquals(4, value);
     }
 
+    @Test
+    void functionComposeTest() {
+        Function<Integer, String> intToString = i -> i.toString();
+        Function<String, String> quote = s -> "'" + s + "'";
+
+        Function<Integer, String> intToQuoteString = quote.compose(intToString);
+        assertEquals("'5'", intToQuoteString.apply(5));
+    }
 }
