b42ef899ea3f3d73ce9b2171ee5ba2d24ffc2775
npee <jmc4664@gmail.com>
2021-12-14 00:18:40 +0900

    Supplier 원시형 테스트

--- test/io/npee/java8/functionalinterface/SupplierInterfaceTestDriveTest.java
+++ test/io/npee/java8/functionalinterface/SupplierInterfaceTestDriveTest.java
@@ -3,6 +3,10 @@ package io.npee.java8.functionalinterface;
 import static org.junit.jupiter.api.Assertions.*;
 
 import io.npee.java8.streams.domain.User;
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
+import java.util.function.IntSupplier;
+import java.util.function.LongSupplier;
 import java.util.function.Supplier;
 import org.junit.jupiter.api.Test;
 
@@ -20,6 +24,21 @@ class SupplierInterfaceTestDriveTest {
         Supplier<User> userSupplier = () -> new User(1L, "user", "user@email.com", "010-xxxx-xxxx");
         System.out.println(userSupplier.get());
         assertEquals("user", userSupplier.get().getName());
+    }
+
+    @Test
+    void supplierPrimitiveTest() {
+        String s = "supplier";
+
+        BooleanSupplier booleanSupplier = () -> s.equals("function");
+        IntSupplier intSupplier = () -> s.length();
+        LongSupplier longSupplier = () -> s.length();
+        DoubleSupplier doubleSupplier = () -> 0.0 + s.length();
+
+        assertFalse(booleanSupplier.getAsBoolean());
+        assertEquals(8, intSupplier.getAsInt());
+        assertEquals(8L, longSupplier.getAsLong());
+        assertEquals(8.0d, doubleSupplier.getAsDouble());
     }
 
 }
