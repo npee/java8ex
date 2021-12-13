50baf66c739e78cb7991c64899ed0832f0060b6e
npee <jmc4664@gmail.com>
2021-12-13 22:29:44 +0900

    Supplier 객체 테스트

--- test/io/npee/java8/functionalinterface/SupplierInterfaceTestDriveTest.java
+++ test/io/npee/java8/functionalinterface/SupplierInterfaceTestDriveTest.java
@@ -2,6 +2,7 @@ package io.npee.java8.functionalinterface;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import io.npee.java8.streams.domain.User;
 import java.util.function.Supplier;
 import org.junit.jupiter.api.Test;
 
@@ -12,6 +13,13 @@ class SupplierInterfaceTestDriveTest {
         // 파라미터가 없음
         Supplier<String> supplier = () -> "supplier";
         assertEquals("supplier", supplier.get());
+    }
+
+    @Test
+    void supplierObjectTest() {
+        Supplier<User> userSupplier = () -> new User(1L, "user", "user@email.com", "010-xxxx-xxxx");
+        System.out.println(userSupplier.get());
+        assertEquals("user", userSupplier.get().getName());
     }
 
 }
