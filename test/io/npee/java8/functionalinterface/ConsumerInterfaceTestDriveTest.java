a6cbee182fb4bd7fa80b9a5686ec31078c930798
npee <jmc4664@gmail.com>
2021-12-16 00:13:30 +0900

    Collection.forEach() 파라미터 테스트

--- test/io/npee/java8/functionalinterface/ConsumerInterfaceTestDriveTest.java
+++ test/io/npee/java8/functionalinterface/ConsumerInterfaceTestDriveTest.java
@@ -2,6 +2,10 @@ package io.npee.java8.functionalinterface;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.Flow.Publisher;
 import java.util.function.Consumer;
 import java.util.function.IntConsumer;
 import org.junit.jupiter.api.Test;
@@ -19,5 +23,12 @@ class ConsumerInterfaceTestDriveTest {
         IntConsumer consumer = n -> System.out.println(n + 10000);
         consumer.accept(1000);
         consumer.accept(2000);
+    }
+
+    @Test
+    void listForEachTest() {
+        List<String> list = Arrays.asList("function", "supplier", "consumer");
+        Consumer<String> consumer = s -> System.out.println(s.toUpperCase());
+        list.forEach(consumer);
     }
 }
