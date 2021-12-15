d78bd30479090534c44535188d647e978205d0dc
npee <jmc4664@gmail.com>
2021-12-16 00:01:45 +0900

    Primitive Consumer 테스트

--- test/io/npee/java8/functionalinterface/ConsumerInterfaceTestDriveTest.java
+++ test/io/npee/java8/functionalinterface/ConsumerInterfaceTestDriveTest.java
@@ -3,6 +3,7 @@ package io.npee.java8.functionalinterface;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.function.Consumer;
+import java.util.function.IntConsumer;
 import org.junit.jupiter.api.Test;
 
 class ConsumerInterfaceTestDriveTest {
@@ -11,5 +12,12 @@ class ConsumerInterfaceTestDriveTest {
     void consumerTest() {
         Consumer<String> consumer = s -> System.out.println(s.toUpperCase());
         consumer.accept("consumer");
+    }
+
+    @Test
+    void primitiveConsumerTest() {
+        IntConsumer consumer = n -> System.out.println(n + 10000);
+        consumer.accept(1000);
+        consumer.accept(2000);
     }
 }
