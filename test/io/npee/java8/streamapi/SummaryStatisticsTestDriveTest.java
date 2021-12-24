a915002a9727a71146ba5f080c16d4d48f035d6d
npee <jmc4664@gmail.com>
2021-12-24 20:50:36 +0900

    통계 summarizingInt 테스트

--- test/io/npee/java8/streamapi/SummaryStatisticsTestDriveTest.java
+++ test/io/npee/java8/streamapi/SummaryStatisticsTestDriveTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.Arrays;
 import java.util.IntSummaryStatistics;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
 class SummaryStatisticsTestDriveTest {
@@ -51,6 +52,20 @@ class SummaryStatisticsTestDriveTest {
                                                .summaryStatistics();
 
         stats.combine(stats2); // 통계 스트림 합치기 (각 통계의 결과끼리 다시 계산)
+
+        System.out.println("Max: " + stats.getMax());
+        System.out.println("Min: " + stats.getMin());
+        System.out.println("Average: " + stats.getAverage());
+        System.out.println("Count: " + stats.getCount());
+    }
+
+    @Test
+    void summarizingIntTest() {
+        List<String> langs = Arrays.asList("java", "kotlin", "haskell", "ruby", "javascript");
+        IntSummaryStatistics stats =
+            langs.stream()
+                 // mapToInt() 와 summaryStatistics()를 합친 형태
+                 .collect(Collectors.summarizingInt(String::length));
 
         System.out.println("Max: " + stats.getMax());
         System.out.println("Min: " + stats.getMin());
