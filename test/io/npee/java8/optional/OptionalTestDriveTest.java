04dc9a79bee89b70fbfdc5c1ee0fb06b86bb5f11
npee <jmc4664@gmail.com>
2022-04-14 22:39:27 +0900

    Optional.ofNullable() 테스트

--- test/io/npee/java8/optional/OptionalTestDriveTest.java
+++ test/io/npee/java8/optional/OptionalTestDriveTest.java
@@ -2,6 +2,7 @@ package io.npee.java8.optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
 
@@ -15,6 +16,16 @@ class OptionalTestDriveTest {
         assertEquals("optional", s);
     }
 
+    @Test
+    void ofNullableTest() {
+        // NPE 방지
+        String string = null;
+        assertThrows(NullPointerException.class, () -> Optional.of(string));
+
+        String string2 = null;
+        Optional<String> nullString = Optional.ofNullable(string2);
+        assertThrows(NoSuchElementException.class, () -> nullString.get());
+    }
 
 
 }
