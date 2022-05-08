944ebbe13582590dd32d96c43775cc42a7976f82
npee <jmc4664@gmail.com>
2022-05-08 12:20:55 +0900

    Stream.findMatch(), findAny() 기본

---
+++ test/io/npee/java8/streamapi/FindMatchTestDriveTest.java
@@ -0,0 +1,21 @@
+package io.npee.java8.streamapi;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+
+class FindMatchTestDriveTest {
+
+    @Test
+    void findTest() {
+        List<String> langs = Arrays.asList("java", "kotlin", "haskell", "ruby", "javascript");
+        Optional<String> firstElement = langs.stream().filter(s -> s.startsWith("j")).findFirst();
+        Optional<String> anyElement = langs.stream().filter(s -> s.startsWith("j")).findAny();
+        firstElement.ifPresent(System.out::println);
+        anyElement.ifPresent(System.out::println);
+    }
+
+}
