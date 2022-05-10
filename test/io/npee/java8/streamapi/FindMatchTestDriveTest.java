614974581e2d5ab856b160276ca02f15cdffd056
npee <jmc4664@gmail.com>
2022-05-10 09:12:00 +0900

    Stream.findMatch(), findAny() 병렬 스트림 테스트

--- test/io/npee/java8/streamapi/FindMatchTestDriveTest.java
+++ test/io/npee/java8/streamapi/FindMatchTestDriveTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class FindMatchTestDriveTest {
@@ -17,5 +18,16 @@ class FindMatchTestDriveTest {
         firstElement.ifPresent(System.out::println);
         anyElement.ifPresent(System.out::println);
     }
+
+    @Test
+    void findParallelTest() {
+        List<String> langs = Arrays.asList("java", "kotlin", "haskell", "ruby", "javascript");
+        Optional<String> firstElement = langs.stream().parallel().filter(s -> s.startsWith("j")).findFirst();
+        Optional<String> anyElement = langs.stream().parallel().filter(s -> s.startsWith("j")).findAny();
+        firstElement.ifPresent(System.out::println);
+        anyElement.ifPresent(System.out::println);
+        assertTrue(anyElement.get().equals("java") || anyElement.get().equals("javascript"));
+    }
+
 
 }
